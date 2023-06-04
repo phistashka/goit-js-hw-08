09c559beaeddfa30fb91d9c12266caf23fe5884f
phistashka <113468642+phistashka@users.noreply.github.com>
2023-06-04 21:25:51 +0300

    Update1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+// 
+// 
+const LOCAL_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+// 
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(storageFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+reloadPage();
+
+function storageFormData(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+function onFormSubmit(e) {
+  e.preventDefault();
+       if (refs.input.value === "" || refs.textarea.value === "") {
+           return alert(`Please fill in all the fields!`);
+       }
+  or
+  const { email, message } = e.currentTarget.elements;
+  console.log({ email: email.value, message: message.value });
+  or
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+  formData = {};
+}
+
+function reloadPage() {
+  if (formData) {
+    let { email, message } = form.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
 
